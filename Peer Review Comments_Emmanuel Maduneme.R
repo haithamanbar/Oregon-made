593c89162c6b0f47fa95a4a9f24074edc3e9e8fa
Emmanuel Maduneme <culatemerald@gmail.com>
2022-11-25 12:38:05 -0800

    Peer Review By Emmanuel

---
+++ Peer Review Comments_Emmanuel Maduneme.R
@@ -0,0 +1,11 @@
+ 
+##Areas of Strengths
+## Coding was organized and easy to read
+## The kable table is a very clear and informative summary of the data
+## The second graph looks really good too. It was elegantly done.
+
+## Learned
+## Line 223: I learnt about using Tag in plots and Transform()
+
+## Improvement
+### Line 201, 214: There's no need for x= when piping.
